3e4b0c350f0773081be588c8c95f2f2ff75ba344
Raja <rajakumar76785@gmail.com>
2024-03-10 19:51:45 +0530

    final

--- db.js
+++ db.js
@@ -4,15 +4,12 @@ require('dotenv').config();
 //const mongoURL = process.env.MONGODB_URL_LOCAL
 const mongoURL  = process.env.MONGODB_URL
 
-mongoose.connect(mongoURL,{
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
+mongoose.connect(mongoURL)
 
 const db= mongoose.connection;
 
 db.on('connected',()=>{
-    console.log('connected to mongodb server');
+    console.log('connected to mongodb server......');
 })
 
 db.on('error',(err)=>{
