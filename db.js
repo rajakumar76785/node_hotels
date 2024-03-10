d6b8d26f3637d2ebeaa530de069b578bdf4c9564
Raja <rajakumar76785@gmail.com>
2024-03-10 19:11:33 +0530

    db

--- db.js
+++ db.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 //const mongoURL = process.env.MONGODB_URL_LOCAL
 const mongoURL  = process.env.MONGODB_URL
+
 const dbconnect =() =>{
     mongoose.connect(mongoURL,{
     }).then(()=>{
